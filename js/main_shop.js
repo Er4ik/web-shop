5714bb423a354e3c6d6953bf70b4aeca8fa5bf2d
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-07 01:55:59 +0300

    update

---
+++ js/main_shop.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenuIcon() {
+    menuIcon.classList.toggle('active')
+}
+
+menuIcon.addEventListener('click', toggleMenuIcon);
