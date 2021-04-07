08da513843a7ea95ab7e682c34fb37988f1665f2
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-08 01:10:39 +0300

    Update

--- js/main_shop.js
+++ js/main_shop.js
@@ -1,7 +1,17 @@
 const menuIcon = document.querySelector('.menu-icon');
-
 function toggleMenuIcon() {
     menuIcon.classList.toggle('active')
 }
-
 menuIcon.addEventListener('click', toggleMenuIcon);
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+document.querySelector('.next').addEventListener('click', function () {
+    offset += 650;
+    if(offset > 1300) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+
