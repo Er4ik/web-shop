71226ea626a6c085abe9248073f077e702eab924
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-12 23:39:39 +0300

    create about-company

--- js/data.js
+++ js/data.js
@@ -17,7 +17,12 @@ const aboutService = {
 }
 
 const aboutProductBack = {
-    'prod1': 'Packed with Innovative Features Including a Super Retina Display, TrueDepth Camera System, Face ID and A13 Bionic Chipwith Neural Engine',
-    'prod2': "Move. Exercise. Stand. Track all the ways you're active. Activity rings show your daily activity.Make it your goal to close them every day.Tokeep you motivated there are awards, personalized coaching, and Activity competitions",
-    'prod3': '13.3-inch (diagonal) LED-backlit display with IPS technology; 2560-by-1600 native resolution at 227 pixels per inch with support for millions of colors',
+    'prod1': ['Packed with Innovative Features Including a Super Retina Display, TrueDepth Camera System, Face ID and A13 Bionic Chipwith Neural Engine'],
+    'prod2': ["Move. Exercise. Stand. Track all the ways you're active. Activity rings show your daily activity.Make it your goal to close them every day.Tokeep you motivated there are awards, personalized coaching, and Activity competitions"],
+    'prod3': ['13.3-inch (diagonal) LED-backlit display with IPS technology; 2560-by-1600 native resolution at 227 pixels per inch with support for millions of colors'],
+}
+
+const aboutCompany = {
+    'head': ['About EAshop'],
+    'text': ['In this section, I will talk about how, as a child, it hit me in the head and I wanted to open my own electronics store, maybe because I was hit with a phone, but not the point. All my life I went to the goal: I studied well, worked hard, developed. And only after entering KPI I was able to fulfill my dream and open my own online store of electronic equipment. For this I am grateful, of course, not to the university, but to my environment, into which I found myself when I came to study. These are the people who have always supported me, helped me in everything. At this stage, I understand that all the changes that take place in life are for the better, and falls help you learn to get up and move on to your dream. The experience gained is the most important thing! Thank you for reading up to this point, I started to write it as a joke to fill this page, but by the end I was so drawn in that I wanted to write something from the heart, enjoy the site :)'],
 }
