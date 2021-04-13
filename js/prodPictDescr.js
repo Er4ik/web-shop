57d6f468f7b3b6e028cc35c6fdb6b8135e2c1d7a
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-13 23:52:43 +0300

    create descrProd

---
+++ js/prodPictDescr.js
@@ -0,0 +1,13 @@
+const headDescrPict1 = document.querySelector('.hd1');
+const headDescrPict2 = document.querySelector('.hd2');
+const headDescrPict3 = document.querySelector('.hd3');
+const DescrPict1 = document.querySelector('.phone-text');
+const DescrPict2 = document.querySelector('.watch-text');
+const DescrPict3 = document.querySelector('.laptop-text');
+
+headDescrPict1.innerHTML = descrProdPict.head1;
+headDescrPict2.innerHTML = descrProdPict.head2;
+headDescrPict3.innerHTML = descrProdPict.head3;
+DescrPict1.innerHTML = descrProdPict.descr1;
+DescrPict2.innerHTML = descrProdPict.descr2;
+DescrPict3.innerHTML = descrProdPict.descr3;
