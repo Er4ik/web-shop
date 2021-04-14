c59bdf5e1d5836f485761f833b2884cdfeb967cc
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-14 12:25:21 +0300

    make scroll-to-pict

--- js/prodPictDescr.js
+++ js/prodPictDescr.js
@@ -1,6 +1,9 @@
 const headDescrPict1 = document.querySelector('.hd1');
 const headDescrPict2 = document.querySelector('.hd2');
 const headDescrPict3 = document.querySelector('.hd3');
+const updownElem1 = document.querySelector('.pct1');
+const updownElem2 = document.querySelector('.pct2');
+const updownElem3 = document.querySelector('.pct3');
 const DescrPict1 = document.querySelector('.phone-text');
 const DescrPict2 = document.querySelector('.watch-text');
 const DescrPict3 = document.querySelector('.laptop-text');
@@ -11,3 +14,32 @@ headDescrPict3.innerHTML = descrProdPict.head3;
 DescrPict1.innerHTML = descrProdPict.descr1;
 DescrPict2.innerHTML = descrProdPict.descr2;
 DescrPict3.innerHTML = descrProdPict.descr3;
+
+let pageYLabel = 0;
+
+updownElem1.onclick = function () {
+    let pageY = window.pageYOffset;
+    if (this.className === 'pct1') {
+        pageYLabel = pageY;
+        window.scrollTo(0, 2500);
+        this.className = 'pct1';
+    }
+}
+
+updownElem2.onclick = function () {
+    let pageY = window.pageYOffset;
+    if (this.className === 'pct2') {
+        pageYLabel = pageY;
+        window.scrollTo(0, 4100);
+        this.className = 'pct2';
+    }
+}
+
+updownElem3.onclick = function () {
+    let pageY = window.pageYOffset;
+        if (this.className === 'pct3') {
+        pageYLabel = pageY;
+        window.scrollTo(0, 3300);
+        this.className = 'pct3';
+    }
+}
