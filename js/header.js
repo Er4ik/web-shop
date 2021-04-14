9bd40a8abcb9efdaa40822c4c6fb3e521f6bfb2f
hack_er <70893070+Er4ik@users.noreply.github.com>
2021-04-14 14:17:17 +0300

    create sign-menu

--- js/header.js
+++ js/header.js
@@ -3,8 +3,12 @@
     const but = document.querySelector('.button');
     const burger = document.querySelector('.burger');
     const menuIcon = document.querySelector('.menu-icon');
+    const signMenuBut = document.querySelector('.but-font3');
+    const signMenu = document.querySelector('.sign-menu');
+    const escSignMenu = document.querySelector('.esc');
     
     let flag = 0;
+    let flagMenu = 0;
     burger.style.marginLeft = 1155 + 'px';
     but.style.display = 'none';
     
@@ -20,5 +24,19 @@
             flag = 0;
         }
     }
+
+    function openSignMenu() {
+        if (flagMenu === 0) {
+            signMenu.style.display = 'flex';
+            flagMenu = 1;
+        }
+        else {
+            signMenu.style.display = 'none';
+            flagMenu = 0;
+        }
+    }
+
     menuIcon.addEventListener('click', toggleMenuIcon);
+    signMenuBut.addEventListener('click', openSignMenu);
+    escSignMenu.addEventListener('click', openSignMenu);
 }
