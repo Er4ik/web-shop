c3c27ff55a007b0799e2264576a98ad88aaea0a1
andreykurzantsev <hawkeye54123_email@gmail.com>
2021-05-28 23:54:29 +0300

    change js code to classes

--- js/phone-catalog.js
+++ js/phone-catalog.js
@@ -1,61 +1,75 @@
+'use strict';
 //icons-rotate
-const icons = document.querySelectorAll('.icon img');
-const positionName = document.querySelectorAll('.p-name');
-const price = document.querySelectorAll('.price');
-const info = document.querySelectorAll('.info');
-const brand = document.querySelectorAll('.brand');
-const memory = document.querySelectorAll('.memory');
-const rate = document.querySelectorAll('.rate');
-let trigger = true;
-for (let key = 0; key < 9; key++) {
-    positionName[key].innerHTML = phonesInfo[`model${key + 1}`];
-    price[key].innerHTML = 'Price: ' + phonesInfo[`price${key + 1}`];
-    brand[key].innerHTML = 'Brand: ' + phonesInfo[`brand${key + 1}`];
-    memory[key].innerHTML = 'Memory size: ' + phonesInfo[`memory${key + 1}`];
-    rate[key].innerHTML = 'Rephresh rate: ' + phonesInfo[`rate${key + 1}`];
-}
+class RotIcon {
+    constructor() {
+        this.icons = document.querySelectorAll('.icon img');
+        this.positionName = document.querySelectorAll('.p-name');
+        this.price = document.querySelectorAll('.price');
+        this.info = document.querySelectorAll('.info');
+        this.brand = document.querySelectorAll('.brand');
+        this.memory = document.querySelectorAll('.memory');
+        this.rate = document.querySelectorAll('.rate');
+    }
 
-const settings = (pict, info, name, opac, trans, opacPict) => {
-    info.style.opacity = opac;
-    name.style.opacity = opac;
-    pict.style.transform = trans;
-    pict.style.opacity = opacPict;
-}
-
-const visibleRotate = (pict, info, name) => {
-    if (info.style.opacity == 0) {
-        settings(pict, info, name, '1', 'rotateY(180deg)', '0.05');
-    } else {
-        settings(pict, info, name, 0, '', 1);
+    infoSet(n) {
+        for (let key = 0; key < n; key++) {
+            this.positionName[key].innerHTML = phonesInfo[`model${key + 1}`];
+            this.price[key].innerHTML = 'Price: ' + phonesInfo[`price${key + 1}`];
+            this.brand[key].innerHTML = 'Brand: ' + phonesInfo[`brand${key + 1}`];
+            this.memory[key].innerHTML = 'Memory size: ' + phonesInfo[`memory${key + 1}`];
+            this.rate[key].innerHTML = 'Rephresh rate: ' + phonesInfo[`rate${key + 1}`];
+        }
+    }
+    settings(pict, info, name, opac, trans, opacPict) {
+        info.style.opacity = opac;
+        name.style.opacity = opac;
+        pict.style.transform = trans;
+        pict.style.opacity = opacPict;
+        return true;
+    }
+    visibleRotate(pict, info, name) {
+        if (info.style.opacity == 0) {
+            this.settings(pict, info, name, '1', 'rotateY(180deg)', '0.05');
+        } else {
+            this.settings(pict, info, name, 0, '', 1);
+        }
     }
 }
-
-icons.forEach((icn, indx) => {
+const Rotor = new RotIcon;
+Rotor.icons.forEach((icn, indx) => {
     icn.addEventListener('click', () => {
-        visibleRotate(icn, info[indx], positionName[indx]);
+        Rotor.infoSet(9);
+        Rotor.visibleRotate(icn, Rotor.info[indx], Rotor.positionName[indx]);
     })
 });
 
 //like-button
-let heart = document.querySelectorAll('.like');
-
-const setUp = (heartpic, link, opa) => {
-    heartpic.src = link;
-    heartpic.style.opacity = opa;
-}
-
-const likeClick = heartpic => {
-    if (heartpic.style.opacity == 0.9) {
-        setUp(heartpic, "pictures/product-icons/heart-clicked.png", 1)
-    } else {
-        setUp(heartpic, "pictures/product-icons/heart.png", 0.9)
+class Like {
+    constructor() {
+        this.heart = document.querySelectorAll('.like');
     }
-}
+    setUp(heartpic, link, opa) {
+        heartpic.src = link;
+        heartpic.style.opacity = opa;
+        return true;
+    }
 
-heart.forEach((item) => {
+    likeClick(heartpic) {
+        if (heartpic.style.opacity == 0.9) {
+            this.setUp(heartpic, "pictures/product-icons/heart-clicked.png", 1)
+            return true;
+        } else {
+            this.setUp(heartpic, "pictures/product-icons/heart.png", 0.9)
+            return false;
+        }
+    }
+
+}
+const likeChange = new Like;
+likeChange.heart.forEach((item) => {
     item.style.opacity = 0.9;
     item.addEventListener('click', () => {
-        likeClick(item);
+        likeChange.likeClick(item);
     })
 });
 
@@ -64,8 +78,9 @@ const filterBox = document.querySelectorAll('.frame');
 const form = document.querySelector('.choice-form');
 
 form.addEventListener('click', (event) => {
-    if (event.target.className !== ('variant')) return false;
-    else {
+    if (event.target.className !== 'variant' && event.target.className !== 'reset') {
+        return false;
+    } else {
         let filterClass = event.target.dataset['f'];
         filterBox.forEach(elem => {
             elem.classList.remove('hide');
