cb2278aade3729492c5378db6c6dff5c67d5b2bc
andreykurzantsev <hawkeye54123_email@gmail.com>
2021-06-02 18:13:12 +0300

    started doing a slider

--- js/phone-catalog.js
+++ js/phone-catalog.js
@@ -125,3 +125,51 @@ class DeskRot {
 const descriptAnim = new DeskRot();
 descriptAnim.changeR();
 descriptAnim.reverseR();
+
+//slider
+const dots = document.querySelectorAll('.dot');
+const dotsArea = document.querySelector('.dots-block');
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev-button');
+const next = document.querySelector('.next-button');
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+  if (n < 1) {
+    slideIndex = slides.length;
+  } else if (n > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('dot-active');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('dot-active');
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+prev.onclick = function () {
+  plusSlides(-1);
+}
+next.onclick = function () {
+  plusSlides(1);
+}
+dotsArea.onclick = function (e) {
+  for (let i = 0; i < dots.length + 1; i++) {
+    if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+      currentSlide(i);
+    }
+  }
+}
