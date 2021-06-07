eb50c2d8496d979e10784d551abdbc6b34753042
Andrey Kurzantsev <hawkeye54123@gmail.com>
2021-06-07 21:34:42 +0300

    create modal window for  shoping cart

---
+++ js/cart.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const openCart = document.querySelector('.modal-cart');
+const closeCart = document.querySelector('.close-but');
+const buyButtons = document.querySelectorAll('.basket');
+
+const closeModalCart = function () {
+    if (openCart.classList.contains('modal-active')) {
+        openCart.classList.remove('modal-active');
+        openCart.classList.add('modal-hide');
+    }
+}
+const openModalCart = function () {
+    openCart.classList.remove('modal-hide');
+    openCart.classList.add('modal-active');
+}
+for (let key of buyButtons) {
+    key.addEventListener('click', () => {
+        openModalCart();
+    });
+}
+closeCart.addEventListener('click', () => {
+    closeModalCart();
+});
